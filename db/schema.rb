6ec0076b774372bc8e1c8cb5e5bb117e07814ca5
rasyidannas <rasshit.dsgn@gmail.com>
2025-10-27 02:21:39 +0000

    2.4 Removing Columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_24_015922) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_27_022048) do
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.string "part_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.decimal "price"
