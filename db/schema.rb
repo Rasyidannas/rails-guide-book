8e7e70ff71b49a52e05e5c0155808625513607dd
rasyidannas <rasshit.dsgn@gmail.com>
2025-12-15 12:21:02 +0000

    3.1.2 Primaary Keys

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_12_014525) do
+ActiveRecord::Schema[8.0].define(version: 2025_12_15_114838) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -23,5 +23,12 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_12_014525) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_products_on_category_id"
+  end
+
+  create_table "users", primary_key: "user_id", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 end
