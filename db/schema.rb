ef1756cc2f3b8e8b64d656a8dc4c3abdd7ac3575
rasyidannas <rasshit.dsgn@gmail.com>
2025-12-18 10:50:19 +0000

    3.1.4. Comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_12_15_114838) do
+ActiveRecord::Schema[8.0].define(version: 2025_12_18_104835) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -22,6 +22,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_12_15_114838) do
     t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "price", precision: 8, scale: 2
+    t.integer "stock_quantity"
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
